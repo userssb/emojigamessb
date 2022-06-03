b27134d12365624cbf09a9a32669c6661b08b388
userssb <95171419+userssb@users.noreply.github.com>
2022-06-03 16:54:54 +0530

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,20 +6,22 @@ const NavBar = props => {
 
   return (
     <nav className="NavBar-cont">
-      <div className="title-logo-cont">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
-          alt="emoji logo"
-          className="emoji-logo"
-        />
-        <h1 className="title">Emoji Game</h1>
-      </div>
-      {isGameInProgress && (
-        <div className="scores-container">
-          <p>score: {currentScore}</p>
-          <p>Top Score: {topScore}</p>
+      <div className="title-logo-score-cont">
+        <div className="title-logo-cont">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            alt="emoji logo"
+            className="emoji-logo"
+          />
+          <h1 className="title">Emoji Game</h1>
         </div>
-      )}
+        {isGameInProgress && (
+          <div className="scores-container">
+            <p>score: {currentScore}</p>
+            <p>Top Score: {topScore}</p>
+          </div>
+        )}
+      </div>
     </nav>
   )
 }
